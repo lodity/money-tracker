945e5d73028239d881ba2c21254ae7f39a4804c6
mrdekan <yeros05040504@gmail.com>
2025-01-04 16:07:11 +0200

    Added: new endpoints

--- src/api/jar.ts
+++ src/api/jar.ts
@@ -3,6 +3,8 @@ import {
   CreateJarRequest,
   CreateJarResponse,
   GetAllJarResponse,
+  DetailedJarResponse,
+  UpdateJarRequest,
 } from '../types/api/jarApi';
 
 export class JarApi {
@@ -10,12 +12,29 @@ export class JarApi {
     return await apiClient.post<CreateJarRequest, CreateJarResponse>(
       `v1/jars`,
       {
-        dto,
+        ...dto,
       },
     );
   }
 
   static async getAll() {
     return await apiClient.get<void, GetAllJarResponse>(`v1/jars`);
+  }
+
+  static async getById(id: number) {
+    return await apiClient.get<void, DetailedJarResponse>(`v1/jars/${id}`);
+  }
+
+  static async update(id: number, dto: UpdateJarRequest) {
+    return await apiClient.patch<UpdateJarRequest, DetailedJarResponse>(
+      `v1/jars/${id}`,
+      {
+        ...dto,
+      },
+    );
+  }
+
+  static async delete(id: number) {
+    return await apiClient.delete<void, DetailedJarResponse>(`v1/jars/${id}`);
   }
 }
---
+++ src/api/store.ts
@@ -0,0 +1,13 @@
+import { apiClient } from '../client/apiClient';
+import { CreateStoreRequest, CreateStoreResponse } from '../types/api/storeApi';
+
+export class StoreApi {
+  static async create(dto: CreateStoreRequest) {
+    return await apiClient.post<CreateStoreRequest, CreateStoreResponse>(
+      `v1/stores`,
+      {
+        ...dto,
+      },
+    );
+  }
+}
